3803cd3533be0a63fa5e59965d959242d4acbd77
Riyan Gonsalves <114173214+optimized-riyan@users.noreply.github.com>
2022-09-23 15:56:54 +0530

    Update concatenation_of_consecutive_binary_numbers.py

--- concatenation_of_consecutive_binary_numbers.py
+++ concatenation_of_consecutive_binary_numbers.py
@@ -1,7 +1,7 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        binNum = ""
+        binNum = ""     # Create enmpty string
         for i in range(1, n + 1):
-            binNum += bin(i)[2:]
-        binNum = "0b" + binNum
+            binNum += bin(i)[2:]    # bin() creates binary num. lead by 0b, [2:] removes 0b
+        binNum = "0b" + binNum      # add 0b to represent it is binary
         return int(binNum, 2) % (10**9 + 7)
